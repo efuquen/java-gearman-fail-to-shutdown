98dd5f39a2aa035acd26c24382c73ef04f7e17d5
Edwin Fuquen <edwin.fuquen@livestream.com>
2012-09-14 16:33:12 -0400

    initial commit

---
+++ test.java
@@ -0,0 +1,35 @@
+import org.gearman.*;
+
+class Main { public static void main(String[] args) {
+
+  /*Gearman gearmanClient = Gearman.createGearman();
+  Gearman gearmanWorker = Gearman.createGearman();*/
+
+  Gearman gearman = Gearman.createGearman();
+  GearmanClient client = gearman.createGearmanClient();
+  GearmanWorker worker = gearman.createGearmanWorker();
+
+  try {
+    client.addServer(gearman.createGearmanServer("localhost", 4730));
+    worker.addServer(gearman.createGearmanServer("localhost", 4730));
+
+    worker.addFunction("test_echo", new GearmanFunction() {
+      public byte[] work(
+        String funcName, byte[] data, GearmanFunctionCallback cb
+      ) {
+        String inputStr = new String(data);
+        return new StringBuffer(inputStr).reverse().toString().getBytes();
+      }
+    });
+
+    GearmanJobReturn gearmanRet = client.submitJob("test_echo", "hello, world".getBytes());
+    while(!gearmanRet.isEOF()) {
+      try {
+        byte[] data = gearmanRet.poll().getData();
+        System.out.println(new String(data));
+      } catch(InterruptedException ex) { }
+    }
+  } finally {
+    gearman.shutdown();
+  }
+}}
